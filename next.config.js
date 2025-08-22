0259cf0a6daa6e73816afea5959b7621e95b457c
Philipp Meier <phi.meier@gmail.com>
2025-08-22 08:53:31 +0200

    fix: configure base path for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
   output: 'export',
   images: {
-    unoptimized: true
+    unoptimized: true,
   },
-  trailingSlash: true
+  trailingSlash: true,
+  basePath: isProd ? '/club98.ch' : undefined,
+  assetPrefix: isProd ? '/club98.ch/' : undefined,
 };
 
 module.exports = nextConfig;
